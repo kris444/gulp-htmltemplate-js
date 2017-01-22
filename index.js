ad04bc943a88dd752b79079cb4b9d73285c747d7
pvmtest <swethusetty@gmail.com>
2017-01-22 11:24:44 +0530

    remove translation keys from templates optimized, append translations to element

--- index.js
+++ index.js
@@ -6,69 +6,72 @@ var fs = require("fs");
 
 const PLUGIN_NAME = 'gulp-htmltemplate-js';
 module.exports = function (options) {
-	var defaultOptions = {
-		'ext': 'js'
-	};
+    var defaultOptions = {
+        'ext': 'js'
+    };
 
-	if (!options) {
-		options = defaultOptions
-	}
-	options.ext = options.ext === undefined ? defaultOptions.ext : options.ext;
-	return through.obj(function (chunk, enc, callback) {
-		if (chunk.isNull()) {
-			callback(null, chunk);
-			return;
-		}
+    if (!options) {
+        options = defaultOptions
+    }
+    options.ext = options.ext === undefined ? defaultOptions.ext : options.ext;
+    return through.obj(function (chunk, enc, callback) {
+        if (chunk.isNull()) {
+            callback(null, chunk);
+            return;
+        }
 
-		if (chunk.isStream()) {
-			var error = new gutil.PluginError(PLUGIN_NAME, 'Streaming error', { showStack: true });
-			callback(error);
-			return;
-		}
+        if (chunk.isStream()) {
+            var error = new gutil.PluginError(PLUGIN_NAME, 'Streaming error', { showStack: true });
+            callback(error);
+            return;
+        }
 
-		if (chunk.isBuffer()) {
-			var cheerioProp = {
-				withDomLvl1: true,
-				normalizeWhitespace: true,
-				xmlMode: false,
-				decodeEntities: true
-			};
-			var $ = cheerio.load(chunk.contents.toString(), cheerioProp);
-			var t = translateTemplate($, options);
-			chunk.contents = new Buffer(_.template('var htmlTemplates = <%= htmlTemplates %>')({ htmlTemplates: JSON.stringify(t) }));
-		}
+        if (chunk.isBuffer()) {
+            var t = translateTemplate(chunk, options);
+            chunk.contents = new Buffer(_.template('var htmlTemplates = <%= htmlTemplates %>')({ htmlTemplates: JSON.stringify(t) }));
+        }
 
-		chunk.path = chunk.path.replace(/\.\w+$/gi, '.' + options.ext);
-		this.push(chunk);
-		return callback();
-	});
+        chunk.path = chunk.path.replace(/\.\w+$/gi, '.' + options.ext);
+        this.push(chunk);
+        return callback();
+    });
 };
 
-function translateTemplate($, options) {
+function translateTemplate(chunk, options) {
+    var cheerioProp = {
+        withDomLvl1: true,
+        normalizeWhitespace: true,
+        xmlMode: false,
+        decodeEntities: true
+    };
+    var $ = cheerio.load(chunk.contents.toString(), cheerioProp);
+    var translations = options.i18nfile ? JSON.parse(fs.readFileSync(options.i18nfile)) : null;
+    var data = {};
+    $('#htmlTemplates').children().each(function (i, element) {
+        var id = $(element).attr('id');
+        if (translations) {
+            for (var lan in translations) {
+                var node = cheerio.load($(element).html());
+                node('*[data-i18n]').each(function (i, e) {
+                    var key = node(e).attr('key');
+                    if (key !== undefined) {
+                        node(e).prepend(translations[lan][key]);
+                    }
+                    node(e).removeAttr('key');
+                    node(e).removeAttr('data-i18n');
+                })
 
-	var translations = options.i18nfile ? JSON.parse(fs.readFileSync(options.i18nfile)) : null;
-	var data = {};
-	$('#htmlTemplates').children().each(function (i, element) {
-		if (translations) {
-			for (var lan in translations) {
-				$('*[data-i18n]').each(function (i, e) {
-					var key = $(e).attr('key');
-					if (key !== undefined) {
-						$(e).text(translations[lan][key]);
-					}
-				})
+                var text = node.html();
+                text = text.replace(/&apos;/g, "'").replace(/&amp;/g, "&").replace(/&gt;/g, ">").replace(/&lt;/g, "<").replace(/&quot;/g, '"');
+                data[id + '-' + lan] = text;
+            }
+        }
+        else {
+            var text = $(element).html();
+            text = text.replace(/&apos;/g, "'").replace(/&amp;/g, "&").replace(/&gt;/g, ">").replace(/&lt;/g, "<").replace(/&quot;/g, '"');
+            data[$(element).attr('id')] = text;
+        }
+    });
 
-				var text = $(element).html();
-				text = text.replace(/&apos;/g, "'").replace(/&amp;/g, "&").replace(/&gt;/g, ">").replace(/&lt;/g, "<").replace(/&quot;/g, '"');
-				data[$(element).attr('id') + '-' + lan] = text;
-			}
-		}
-		else {
-			var text = $(element).html();
-			text = text.replace(/&apos;/g, "'").replace(/&amp;/g, "&").replace(/&gt;/g, ">").replace(/&lt;/g, "<").replace(/&quot;/g, '"');
-			data[$(element).attr('id')] = text;
-		}
-	});
-
-	return data;
+    return data;
 }
